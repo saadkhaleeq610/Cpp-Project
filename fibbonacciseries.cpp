17fb203a141ec8fa5e562bedcaf5abfcdcf06d77
saadkhaleeq610 <58370495+saadkhaleeq610@users.noreply.github.com>
2023-11-20 18:53:52 +0500

    Add files via upload

---
+++ fibbonacciseries.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+void fib(int n){
+    int term1 = 0;
+    int term2 = 1;
+    int nterm;
+    
+    for (int i = 1; i <= n; i++)
+    {
+        cout<<term1<<endl;
+        nterm = term1 + term2;
+        term1=term2;
+        term2=nterm;
+    }
+    
+}
+
+int main(){
+
+    int n;
+    cout<<"Enter a number: "<<endl;
+    cin>>n;
+
+    fib(n);
+
+return 0;
+}
