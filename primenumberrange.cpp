8b915221355732703236eb6ef0dabf189d845d96
saadkhaleeq610 <58370495+saadkhaleeq610@users.noreply.github.com>
2023-11-20 18:45:22 +0500

    Add files via upload

---
+++ primenumberrange.cpp
@@ -0,0 +1,32 @@
+//This program prints prime numbers between two numbers
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+bool isprime(int n){
+        for (int i = 2; i <= sqrt(n); i++)
+        {
+            if (n%i==0)
+            {
+                return false;
+            }
+        return true;    
+        }
+}
+
+int main(){
+
+    int a, b;
+    cin>>a>>b;
+    
+    for (int i = a; i <= b; i++)
+    {
+        if (isprime(i))
+        {
+            cout<<i<<endl;
+        }
+        
+    }
+    
+return 0;
+}
